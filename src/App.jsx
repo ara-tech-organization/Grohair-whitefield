79a3a93c130fc3721946b92806e1480c53ba5c4b
Samyu <samyuktha.ara@gmail.com>
2026-01-05 18:12:13 +0530

    0501

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <BrowserRouter basename="/Grohair-whiteField">
+    <BrowserRouter basename="/Grohair-whitefield/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
